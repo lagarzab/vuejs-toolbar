61d10ce130f18f3212597f805145f6c4b2e24ac9
Bryan <lagarzab@gmail.com>
2020-03-30 22:00:08 -0400

    Initial build of toolbar component

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# vuejs-toolbar
-A (very) simple vuejs toolbar component
+# Toolbar
+A simple toolbar component used to hold site navigation, logos, titles etc for your site.
+
+## Usage
+The toolbar component exports multiple components. The default export is the `toolbar` component.
+You can also use the `toolbarButton` which already contains some default styling to work within the `toolbar` itself.
+
+### `toolbar` Component
+`toolbar` has 3 slots available for placement of your links, buttons, logos and text.
+- `left`: flex item, 1/3 width, left aligned
+- `middle`: flex item, 1/3 width, center aligned
+- `right`: flex item, 1/3 width, right aligned
+
+### `toolbarButton` Component
+`toolbarButton` has one prop and one slot (default)
+- `href`: default is `#`.
+- `default` slot: use this to pass the label for your link/button.
+
+
+Here is an example on how to use these components:
+```
+    <toolbar>
+        <template #left>
+            <img src='logo.png' />
+        </template>
+        <template #right>
+            <toolbar-button href='/another.htm'>Button Name</toolbar-button>
+        </template>
+    </toolbar>
+```
---
+++ components/toolbar.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class='toolbar'>
+        <div class='toolbar-column-left'>
+            <div>
+                <slot name='left'>
+                    <q-icon name='branding_watermark' style='font-size:50px;'></q-icon>
+                    Logo
+                </slot>
+            </div>
+        </div>
+
+        <div class='toolbar-column-middle'>
+            <slot name='middle'></slot>
+        </div>
+
+        <div class='toolbar-column-right '>
+            <slot name='right'>
+            </slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'toolbar',
+    data () {
+        return {
+
+        }
+    },
+}
+</script>
+
+<style lang='scss' scoped>
+    .toolbar {
+        display: flex;
+        flex-direction: row;
+        width:100%;
+        background-color:rgba(red, .3);
+    }
+    .toolbar-column {
+        @extend .toolbar;
+        flex-grow: 1;
+        width:33%;
+    }
+    .toolbar-column-left {
+        @extend .toolbar-column;
+        justify-content: flex-start;
+        align-items:center;
+        padding: 0px 10px;
+    }
+    .toolbar-column-middle {
+        @extend .toolbar-column;
+        justify-content: center;
+        align-items:center;
+        padding: 0px 10px;
+    }
+    .toolbar-column-right {
+        @extend .toolbar-column;
+        justify-content: flex-end;
+        align-items:center;
+        padding: 0px 10px;
+    }
+
+</style>
